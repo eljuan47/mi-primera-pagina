6c6ed45dba7bc13370a51c8740d66f5750ad9872
maicol <juansebastianmaestre@uparsistem.edu.co>
2024-12-03 08:14:51 -0500

    mi primer commit

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 console.log("estoy en js")
+function fSumar(){
+  let num1 = parseInt(n1.value)
+  let num2 = parseInt(n2.value)
+  //console.log(typeof(num1))
+  let resultado = "La suma es: " + (num1+num2)
+  Document
+  console.log(resultado)
+}
+
+function fRestar(){
+  let num1 = parseInt(n1.value)
+  let num2 = parseInt(n2.value)
+  console.log("La resta es: " + (num1-num2))
+}
+function fMultiplicar(){
+  let num1 = parseInt(n1.value)
+  let num2 = parseInt(n2.value)
+  console.log("La multiplicacion es: " + (num1*num2))
+}
+function fDividir(){
+  let num1 = parseInt(n1.value)
+  let num2 = parseInt(n2.value)
+  if(num2===0)
+  {console.log("La division entre cero (0) No es posible")}
+  else{console.log("La division es: "+(num1/num2))}
+}
